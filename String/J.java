33548de1b70d051681a2960c2bb6a9d59d5c3d77
Kshitij Jain <kshitijjain6074@gmail.com>
2026-01-21 22:23:28 +0530

    LC. 3816. Lexicographically Smallest String After Deleting Duplicate Characters

---
+++ String/J.java
@@ -0,0 +1,7 @@
+// LC. 3816. Lexicographically Smallest String After Deleting Duplicate Characters: https://leetcode.com/problems/lexicographically-smallest-string-after-deleting-duplicate-characters/description/
+
+public class J{
+    // Approach-1:
+    // TC = O(), SC = O()
+    
+}
